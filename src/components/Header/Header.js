0ef47e5358c5ae3394e2576449aeee4c333159f1
Moin Khan <nurmoin05@gmail.com>
2022-07-22 11:53:35 +0600

    header structed created

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import './Header.scss';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png';
 
 const Header = () => {
     return (
         <div className="Header">
             <div className="inner__header container">
                 <div className="header__content">
-                    <h1>this is header</h1>
+                    <div className="header__menu">
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/">Docs</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="header__logo">
+                        <Link to="/">
+                            <img src={logo} alt="Logo" />
+                        </Link>
+                    </div>
+                    <div className="header__menu">
+                        <ul>
+                            <li>
+                                <Link to="/">white paper</Link>
+                            </li>
+                            <li>
+                                <Link to="/">light paper</Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
