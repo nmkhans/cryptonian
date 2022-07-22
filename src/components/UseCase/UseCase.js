d8a495ba531b638b67d46e9dac824a4140ad30c1
Moin Khan <nurmoin05@gmail.com>
2022-07-22 22:25:09 +0600

    useCase section completed

---
+++ src/components/UseCase/UseCase.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './UseCase.scss';
+import play from '../../assets/play.png';
+import useCase from '../../assets/usecase.png';
+
+const UseCase = () => {
+    return (
+        <div className="UseCase">
+            <div className="inner__usecase container">
+                <div className="usecase__content">
+                    <div className="usecase__text">
+                        <div className="usecase__title">
+                            <h2>Use cases for CNFT</h2>
+                        </div>
+                        <div className="usecase__shorting">
+                            <ul>
+                                <li>
+                                    <button>Shorting</button>
+                                </li>
+                                <li>
+                                    <button>Leverage</button>
+                                </li>
+                                <li>
+                                    <button>Farming</button>
+                                </li>
+                                <li>
+                                    <button>Safe buy</button>
+                                </li>
+                                <li>
+                                    <button>Transferable Loan</button>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="usecase__description">
+                            <p>Shorting is the act of selling the cryptocurrency in the hope that it falls in value and you can buy it back at a lower price.</p>
+                        </div>
+                        <div className="usecase__action">
+                            <button>
+                                <img src={play} alt="Launch application" />
+                            </button>
+                            <p>Launch Application</p>
+                        </div>
+                    </div>
+                    <div className="usecase__feature">
+                        <img src={useCase} alt="Usecase Feature Img" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default UseCase;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Home.scss';
 import Banner from './../../components/Banner/Banner';
+import UseCase from './../../components/UseCase/UseCase';
 
 const Home = () => {
     return (
         <div className="Home">
             <Banner />
+            <UseCase />
         </div>
     );
 };
