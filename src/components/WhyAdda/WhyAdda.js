a48a86988b12bab819e1ae4d97ec3d2ba0dcd3cb
Moin Khan <nurmoin05@gmail.com>
2022-07-23 21:25:20 +0600

    why adaa section added

---
+++ src/components/WhyAdda/WhyAdda.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './WhyAdda.scss';
+import img from '../../assets/whyadaa.png';
+
+const WhyAdda = () => {
+    return (
+        <div className="WhyAdda">
+            <div className="inner__whyadda container">
+                <div className="whyadda__title">
+                    <h2>Why Aada?</h2>
+                </div>
+                <div className="whyadda__content">
+                    <div className="whyadda__feature">
+                        <img src={img} alt="Why Adaa" />
+                    </div>
+                    <div className="whyadda__text">
+                        <h3>NFT-Bonds</h3>
+                        <p>Borrowers loan or Lenders deposit is locked into NFT-Bond and can be redeemed by anyone who provides an NFT and its condition. This creates a totally new financial instrument that can be transferred, sold or staked on DeFi.</p>
+                    </div>
+                    <div className="whyadda__list">
+                        <ul>
+                            <li><h4>NFT-Bonds</h4></li>
+                            <li>Stable and sustainable returns</li>
+                            <li>Non - Custodial</li>
+                            <li>Transparent DAO</li>
+                            <li>Utilizable token</li>
+                            <li>eUTXO empowered</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default WhyAdda;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './Home.scss';
 import Banner from './../../components/Banner/Banner';
 import UseCase from './../../components/UseCase/UseCase';
+import WhyAdda from '../../components/WhyAdda/WhyAdda';
 
 const Home = () => {
     return (
         <div className="Home">
             <Banner />
             <UseCase />
+            <WhyAdda />
         </div>
     );
 };
